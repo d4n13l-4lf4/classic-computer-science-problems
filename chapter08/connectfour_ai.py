975a76a53031f2f85ec0efe6db1770d982b1e40f
Daniel Aucatoma <d.aucatoma96@gmail.com>
2020-07-04 23:02:26 -0500

    Minimax para Connect Four

---
+++ chapter08/connectfour_ai.py
@@ -0,0 +1,34 @@
+from chapter08.minimax import find_best_move
+from chapter08.connectfour import C4Board
+from chapter08.board import Move, Board
+
+board: Board = C4Board()
+
+
+def get_player_move() -> Move:
+    player_move: Move = Move(-1)
+    while player_move not in board.legal_moves:
+        play: int = int(input("Enter a legal column (0-6):"))
+        player_move = Move(play)
+    return player_move
+
+if __name__ == '__main__':
+    while True:
+        human_move: Move = get_player_move()
+        board = board.move(human_move)
+        if board.is_win:
+            print("Human wins!")
+            break
+        if board.is_draw:
+            print("Draw!")
+            break
+        computer_move: Move = find_best_move(board, 3)
+        print(f"Computer move is {computer_move}")
+        board = board.move(computer_move)
+        print(board)
+        if board.is_win:
+            print("Computer wins!")
+            break
+        elif board.is_draw:
+            print("Draw!")
+            break
